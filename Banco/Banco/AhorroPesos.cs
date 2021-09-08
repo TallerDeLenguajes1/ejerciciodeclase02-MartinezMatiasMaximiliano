ec1560384a3eca89e6690d9390bf07b684cb5b43
matias maximiliano martinez <matumartinez9@hotmail.com>
2021-09-08 17:51:27 -0300

    piedad?

--- Banco/Banco/AhorroPesos.cs
+++ Banco/Banco/AhorroPesos.cs
@@ -8,5 +8,24 @@ namespace Banco
 {
     public class AhorroPesos : Cuenta
     {
+        public void Extraccion(int monto, TipoDeExtraccion tipo)
+        {
+            float insertado = Convert.ToInt32(Console.ReadLine());
+            if (tipo == TipoDeExtraccion.CajeroAutomatico)
+            {
+                if (insertado > 10000)
+                {
+                    Console.WriteLine("no se puede retirar mas de 10000 por cajero automatico");
+                }
+                else
+                {
+                    this.Cantidad -= monto;
+                }
+            }
+            else
+            {
+                this.Cantidad -= monto;
+            }
+        }
     }
 }
